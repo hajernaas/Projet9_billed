201880b7ea1cf4443361a53e540a3d8cdd1cd0ac
hajernaas <hnaas2702@gmail.com>
2023-12-21 18:51:50 +0100

    first commit

---
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -0,0 +1,92 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import "@testing-library/jest-dom";
+
+import { screen, waitFor, within } from "@testing-library/dom";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import BillsUI from "../views/BillsUI.js";
+import { bills } from "../fixtures/bills.js";
+import { ROUTES_PATH } from "../constants/routes";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+//import mockedStore from "../__mocks__/store";
+import router from "../app/Router.js";
+import Bills from "../containers/Bills.js";
+import { log } from "console";
+
+//jest.mock("../app/store", () => mockedStore);
+
+/*
+beforeAll(() => {
+	Object.defineProperty(window, "localStorage", { value: localStorageMock });
+
+	window.localStorage.setItem(
+		"user",
+		JSON.stringify({
+			type: "Employee",
+		})
+	);
+});
+
+afterEach(() => {
+	jest.clearAllMocks();
+});*/
+
+const onNavigate = (pathname) => {
+	document.body.innerHTML = ROUTES({ pathname });
+};
+
+//Tests pour s'assurer que la page bills fonctionne correctement pour un utilisateur connecté en tant qu'employé.
+describe("Given I am connected as an employee", () => {
+	describe("When I am on Bills Page", () => {
+		// Test pour vérifier que l'icône windows en disposition verticale est activé
+		test("Then bill icon in vertical layout should be highlighted", async () => {
+			Object.defineProperty(window, "localStorage", {
+				value: localStorageMock,
+			});
+			window.localStorage.setItem(
+				"user",
+				JSON.stringify({
+					type: "Employee",
+				})
+			);
+
+			// Créer l'élément root et l'ajoute au corps du document
+			const root = document.createElement("div");
+			root.setAttribute("id", "root");
+			document.body.append(root);
+
+			//Appeler le routeur et accéder à la page des bills (mes notes de frais)
+			router();
+			window.onNavigate(ROUTES_PATH.Bills);
+
+			//to-do write expect expression
+			const WindowIcon = screen.getByTestId("icon-window");
+			await waitFor(() => WindowIcon);
+		});
+
+		// Test pour vérifier que les notes de frais s'affichent par ordre décroissant
+		test("Then bills should be ordered from earliest to latest", () => {
+			//Afficher l'interface BillsUI (mes notes de frais)
+			document.body.innerHTML = BillsUI({ data: bills });
+			//Récupèrer toutes les dates et les trie par ordre décroissant
+			const dates = screen
+				.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i)
+				.map((a) => a.innerHTML);
+			log("dates", dates);
+			const antiChrono = (a, b) => (a < b ? 1 : -1);
+			const datesSorted = [...dates].sort(antiChrono);
+			//Vérifier que les dates récupérées sont égales aux dates triées
+			expect(dates).toEqual(datesSorted);
+		});
+	});
+	// test pour vérifier que notre Loader s'affiche bien.
+	describe("When I am on bills page but it is loading", () => {
+		test("Then, Loading page should be rendered", () => {
+			document.body.innerHTML = BillsUI({ loading: true });
+			expect(screen.getAllByText("Loading...")).toBeTruthy();
+		});
+	});
+});
---
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -0,0 +1,98 @@
+import { ROUTES_PATH } from "../constants/routes.js";
+import Logout from "./Logout.js";
+
+export default class NewBill {
+	constructor({ document, onNavigate, store, localStorage }) {
+		this.document = document;
+		this.onNavigate = onNavigate;
+		this.store = store;
+		const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`);
+		formNewBill.addEventListener("submit", this.handleSubmit);
+		const file = this.document.querySelector(`input[data-testid="file"]`);
+		file.addEventListener("change", this.handleChangeFile);
+		this.fileUrl = null;
+		this.fileName = null;
+		this.billId = null;
+		new Logout({ document, localStorage, onNavigate });
+	}
+
+	//Appeler cette fonction quand l'utilisateur sélectionne un fichier à télécharger.
+
+	handleChangeFile = (e) => {
+		e.preventDefault();
+		//Récupèrer le fichier sélectionné et vérifie son extension
+		const file = this.document.querySelector(`input[data-testid="file"]`).files[0];
+		console.log("files", file);
+		// BUG corrigé - Empêcher la saisie d'un document qui a une extension différente de jpg, jpeg ou png.
+		//Regex pour les extensions autorisées
+		const allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i; //utiliser le modificateur "i" qui spécifie une correspondance sans distinction de casse
+		//Effectuer une recherche insensible à la casse pour « les extensions autorisées» dans le nom de fichier
+		if (!allowedExtensions.exec(file.name)) {
+			// Afficher un message d'alerte si l'extension n'est pas autorisée
+			alert("Type de fichier invalide , Veuillez sélectionner un fichier au format JPEG ou PNG.");
+			e.target.value = ""; // vider le champs input (file)
+			return false;
+		}
+
+		//Extraire le nom du fichier et créer un nouvel objet FormData à envoyer à l'API
+		const filePath = e.target.value.split(/\\/g);
+		const fileName = filePath[filePath.length - 1];
+		const formData = new FormData();
+		const email = JSON.parse(localStorage.getItem("user")).email;
+		formData.append("file", file);
+		formData.append("email", email);
+		//Envoyer les données à l'API et mettre à jour l'ID, l'URL et le nom du fichier choisi
+		this.store
+			.bills()
+			.create({
+				data: formData,
+				headers: {
+					noContentType: true,
+				},
+			})
+			.then(({ fileUrl, key }) => {
+				console.log(fileUrl);
+				this.billId = key;
+				this.fileUrl = fileUrl;
+				this.fileName = fileName;
+			})
+			.catch((error) => console.error(error));
+	};
+
+	//Soumission du formulaire,
+	handleSubmit = (e) => {
+		e.preventDefault();
+
+		const email = JSON.parse(localStorage.getItem("user")).email;
+
+		// Récupèrer les valeurs des champs du formulaire et créer un nouvel objet de bill ( note de frais )
+		const bill = {
+			email,
+			type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
+			name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
+			amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
+			date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
+			vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+			pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
+			commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
+			fileUrl: this.fileUrl,
+			fileName: this.fileName,
+			status: "pending",
+		};
+		this.updateBill(bill);
+		this.onNavigate(ROUTES_PATH["Bills"]);
+	};
+
+	// not need to cover this function by tests
+	updateBill = (bill) => {
+		if (this.store) {
+			this.store
+				.bills()
+				.update({ data: JSON.stringify(bill), selector: this.billId })
+				.then(() => {
+					this.onNavigate(ROUTES_PATH["Bills"]);
+				})
+				.catch((error) => console.error(error));
+		}
+	};
+}
